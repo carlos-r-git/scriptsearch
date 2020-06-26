db1e58cd7d22af089464570a6a56c5d53bb0c292
Carlos <cr1056@york.ac.uk>
2020-06-26 17:50:21 +0100

    used pattern instead of str_detect

--- R/searchdir.R
+++ R/searchdir.R
@@ -1,7 +1,6 @@
 searchdir <- function(dir, filetypes) {
 
-  files <- list.files(dir, recursive = TRUE, include.dirs = TRUE)
-  files <- files[stringr::str_detect(files, paste(filetypes, collapse = "|"))]
+  files <- list.files(dir, recursive = TRUE, include.dirs = TRUE, pattern = paste(filetypes, collapse = "|"))
   return(files)
 
 }
