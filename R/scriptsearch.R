236890f669aad613a6d4cbcb88843bc34bb4c884
Carlos <cr1056@york.ac.uk>
2020-06-26 13:36:28 +0100

    added third column to data for searchterms

--- R/scriptsearch.R
+++ R/scriptsearch.R
@@ -2,12 +2,13 @@ scriptsearch <- function(dir, searchterms, filetypes = c(".R", ".rmd")) {
 
   files <- searchdir(dir = dir, filetypes = filetypes)
 
-  data <- data.frame(1:length(files),2)
-  colnames(data) <- c("Path", "Hits")
+  data <- data.frame(1:length(files),0,0)
+  colnames(data) <- c("Path", "Hits", "Search Terms")
+  data[,3] <- searchterms
 
   for (i in 1:length(files)) {
 
-    tempdata <- readfile(dir = dir, files = files, i = i)
+    tempdata <- readfile(file = paste0(dir,"/",files[i]))
 
     temppos <- findlines(tempdata = tempdata, searchterms = searchterms)
 
