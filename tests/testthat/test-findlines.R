a09c65ebaf54d4eb982882a8dbc9ab404775f62c
Carlos <cr1056@york.ac.uk>
2020-07-02 11:18:41 +0100

    add tests for findlines

--- tests/testthat/test-findlines.R
+++ tests/testthat/test-findlines.R
@@ -1,3 +1,4 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("finds lines", {
+  expect_equal(findlines(c("x", "y"), "x"), 1)
+  expect_equal(findlines(c("x", "y"), "z"), integer(0))
 })
