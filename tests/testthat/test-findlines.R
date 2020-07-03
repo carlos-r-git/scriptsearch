9d656d70bd39bfc066aaa44b8a6e9f62b508b821
Carlos <cr1056@york.ac.uk>
2020-07-03 12:54:51 +0100

    adding tests

--- tests/testthat/test-findlines.R
+++ tests/testthat/test-findlines.R
@@ -1,4 +1,5 @@
 test_that("finds lines", {
   expect_equal(findlines(c("x", "y"), "x"), 1)
+  expect_equal(findlines(c("x", "y"), c("x", "y")), c(1, 2))
   expect_equal(findlines(c("x", "y"), "z"), integer(0))
 })
--- tests/testthat/test-searchdir.R
+++ tests/testthat/test-searchdir.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("filters files from string", {
+  expect_equal(searchdir(system.file("extdata", package = "scriptsearch"), "test"), c("test1.txt", "test2.txt"))
+  expect_equal(searchdir(system.file("extdata", package = "scriptsearch"), "1"),"test1.txt")
+  expect_equal(searchdir(system.file("extdata", package = "scriptsearch"), "3"), character(0))
 })
