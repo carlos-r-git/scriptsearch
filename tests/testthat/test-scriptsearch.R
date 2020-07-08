21250b49165de3d92adfc37adf48deecc1f5a3a6
Carlos <cr1056@york.ac.uk>
2020-07-08 23:33:21 +0100

    make tests actually work

--- tests/testthat/test-scriptsearch.R
+++ tests/testthat/test-scriptsearch.R
@@ -1,7 +1,16 @@
 test_that("finds files", {
-  data <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
-                     "Hits" = c("TRUE", "TRUE"),
-                     "Searchterms" = c("x", "x"))
 
-  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "x", filetypes = "\\.txt$"), data)
+  data1 <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
+                     "Hits" = c("TRUE", "TRUE"),
+                     "Searchterms" = c("x", "x"),
+                     stringsAsFactors = FALSE)
+
+  data2 <- data.frame("Path" = paste(system.file("extdata/test1.txt", package = "scriptsearch")),
+                     "Hits" = "TRUE",
+                     "Searchterms" = "z",
+                     stringsAsFactors = FALSE)
+
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "x", filetypes = "\\.txt$"), data1)
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "z", filetypes = "\\.txt$"), data2)
+
 })
