6e4a782a1fcce30a9932579031e37a8ea3ce3757
Carlos <cr1056@york.ac.uk>
2020-07-08 23:23:16 +0100

    add test for scirptsearch

--- tests/testthat/test-scriptsearch.R
+++ tests/testthat/test-scriptsearch.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("finds files", {
+  data <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
+                     "Hits" = c("TRUE", "TRUE"),
+                     "Searchterms" = c("x", "x"))
+
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "x", filetypes = "\\.txt$"), data)
 })
