17e17f1bc24641dedec731a252bcf3b637f29ca8
Carlos <cr1056@york.ac.uk>
2020-07-09 12:24:12 +0100

    broke tests

--- tests/testthat/test-scriptsearch.R
+++ tests/testthat/test-scriptsearch.R
@@ -6,34 +6,35 @@ test_that("finds files", {
                      stringsAsFactors = FALSE)
 
   expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "x", filetypes = "\\.txt$"), data1)
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "x", filetypes = "\\.R$"), "No files found in directory.")
 
 })
 
 test_that("removes negatives", {
 
-  data2 <- data.frame("Path" = paste(system.file("extdata/test1.txt", package = "scriptsearch")),
+  data1 <- data.frame("Path" = paste(system.file("extdata/test1.txt", package = "scriptsearch")),
                       "Hits" = 1,
                       "Searchterms" = "z",
                       stringsAsFactors = FALSE)
 
-  data3 <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
+  data2 <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
                       "Hits" = c(1, 0),
                       "Searchterms" = c("z", "z"),
                       stringsAsFactors = FALSE)
 
-  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "z", rm = T, filetypes = "\\.txt$"), data2)
-  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "z", rm = F, filetypes = "\\.txt$"), data3)
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "z", rm = T, filetypes = "\\.txt$"), data1)
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "z", rm = F, filetypes = "\\.txt$"), data2)
 
 })
 
 test_that("no hits in any files", {
 
-  data4 <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
+  data1 <- data.frame("Path" = c(paste(system.file("extdata/test1.txt", package = "scriptsearch")), paste(system.file("extdata/test2.txt", package = "scriptsearch"))),
                       "Hits" = c(0, 0),
                       "Searchterms" = c("w", "w"),
                       stringsAsFactors = FALSE)
 
-  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "w", rm = F, filetypes = "\\.txt$"), data4)
+  expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "w", rm = F, filetypes = "\\.txt$"), data1)
   expect_equal(scriptsearch(system.file("extdata", package = "scriptsearch"), "w", filetypes = "\\.txt$"), "No files containing search terms found.")
 
 })
