808af1a8c4a5428145fda3af2e4e486ab23809a3
Carlos <cr1056@york.ac.uk>
2020-08-11 01:56:46 +0100

    fix test

--- tests/testthat/test-showlines.R
+++ tests/testthat/test-showlines.R
@@ -1,9 +1,9 @@
 test_that("shows lines", {
 
   data1 <- data.frame("Line" = c(1, 2),
-                      "Text" = "x", "y",
+                      "Text" = c("x", "y"),
                       stringsAsFactors = FALSE)
 
-  expect_equal(showlines(scriptsearch(system.file("extdata", package = "scriptsearch"), c("x", "y"), filetypes = "\\.txt$"), 1), data1)
+  expect_equal(showlines(scriptsearch(system.file("extdata", package = "scriptsearch"), c("x", "y"), filetypes = "\\.txt$"), 1, open = FALSE), data1)
 
 })
